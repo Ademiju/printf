e2bd9eb7b71946dcbeed622b1e98711afa1b8e10
Ademiju Taiwo <81045677+Ademiju@users.noreply.github.com>
2022-10-15 12:40:17 +0100

    Update _char_printers.c

--- _char_printers.c
+++ _char_printers.c
@@ -8,23 +8,20 @@
  */
 static int hex_print(char c)
 {
-int count;
-char diff = 'A' - ':';
-char d[2];
-d[0] = c / 16;
-d[1] = c % 16;
-for (count = 0; count < 2; count++)
-{
-if (d[count] >= 10)
-_putchar('0' + diff + d[count]);
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
 
-else
-
-_putchar('0' + d[count]);
-
-}
-
-return (count);
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
 }
 
 /**
@@ -35,10 +32,10 @@ return (count);
  */
 int _print_char(va_list args)
 {
-char ch = (char)va_arg(args, int);
+	char ch = (char)va_arg(args, int);
 
-_putchar(ch);
-return (1);
+	_putchar(ch);
+	return (1);
 }
 
 /**
@@ -49,15 +46,16 @@ return (1);
  */
 int _print_str(va_list args)
 {
-int count;
-char *str = va_arg(args, char *);
-if (str == NULL)
-str = "(null)";
-for (count = 0; str[count]; count++)
-{
-_putchar(str[count]);
-}
-return (count);
+	int count;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
 /**
  * _print_str_nonprintable - prints a string and nonprintable
@@ -68,42 +66,28 @@ return (count);
  */
 int _print_str_nonprintable(va_list args)
 {
-unsigned int i;
-int count = 0;
-char *str = va_arg(args, char *);
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(args, char *);
 
-if (str == NULL)
-
-str = "(null)";
-for (i = 0; str[i]; i++)
-{
-if (str[i] < 32 || str[i] >= 127)
-
-{
-
-_putchar('\\');
-
-_putchar('x');
-
-count += 2;
-
-count += hex_print(str[i]);
-
-}
-
-else
-
-{
-
-_putchar(str[i]);
-
-count++;
-
-}
-
-}
-
-return (count);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
 }
 /**
  * _print_str_reverse - prints a string in reverse
@@ -113,26 +97,18 @@ return (count);
  */
 int _print_str_reverse(va_list args)
 {
-char *str;
-int i, count = 0;
+	char *str;
+	int i, count = 0;
 
-str = va_arg(args, char *);
-
-if (str == NULL)
-
-str = ")llun(";
-
-for (i = 0; str[i]; i++);
-
-for (i -= 1; i >= 0; i--)
-
-{
-
-_putchar(str[i]);
-
-count++;
-
-}
-
-return (count);
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
 }
